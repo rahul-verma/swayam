348134a5e5e55ad68f9716c9c7f77cb0095ba2eb
Rahul Verma <rahulverma81@gmail.com>
2025-06-23 21:57:21 +0530

    Added logic for function calling

--- swayam/core/llm/model.py
+++ swayam/core/llm/model.py
@@ -12,30 +12,37 @@ class _OpenAIModel:
         self.__client = openai.OpenAI()
         self.__temperature = temperature
         self.__messages = []
-        
-    def run_prompt(self, prompt, temperature=None):
-        if temperature is None:
-            temperature = self.__temperature
-        self.__messages.append({"role": "user", "content": prompt.text})
-        response = self.__client.chat.completions.create(
-            model=self.__model,
-            messages=self.__messages,
-            temperature=self.__temperature
-        )
-        return response.choices[0].message.content
     
-    def run_messages(self, messages, retain_context=True, temperature=None):
+    def run_messages(self, messages, retain_context=True, temperature=None, functions=None, function_call="auto", return_content=True):
         if retain_context:
             self.__messages.extend(messages)
             messages = self.__messages
         if temperature is None:
             temperature = self.__temperature
-        response = self.__client.chat.completions.create(
-            model=self.__model,
-            messages=messages,
-            temperature=self.__temperature
-        )
-        return response.choices[0].message.content       
+            
+        response = None
+        if functions:
+            response = self.__client.chat.completions.create(
+                model=self.__model,
+                messages=messages,
+                temperature=temperature,
+                functions=functions,
+                function_call=function_call
+            )
+        else:
+            response = self.__client.chat.completions.create(
+                model=self.__model,
+                messages=messages,
+                temperature=temperature,
+            )
+    
+        if return_content:
+            return response.choices[0].message.content
+        else:
+            return response.choices[0].message 
+        
+    def run_prompt(self, prompt, temperature=None, functions=None, function_call="auto"):
+        return self.run_messages([{"role": "user", "content": prompt.text}], temperature=temperature, functions=functions, function_call=function_call)
     
     def clear_context(self):
         self.__messages = []
